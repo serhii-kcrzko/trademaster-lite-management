bd3f60e6b5ec05d2d26e245f3b3c1368dc2d48d8
Serhii Kocherezhko <Serhii Kocherezhko>
2017-12-11 11:55:55 +0200

    Update Heroku config

--- src/config.js
+++ src/config.js
@@ -23,7 +23,7 @@ const config = {
   all: {
     env: process.env.NODE_ENV || 'development',
     root: path.join(__dirname, '..'),
-    port: process.env.PORT || 9000,
+    port: process.env.PORT || 8080,
     ip: process.env.IP || '127.0.0.1',
     masterKey: requireProcessEnv('MASTER_KEY'),
     mongo: {
